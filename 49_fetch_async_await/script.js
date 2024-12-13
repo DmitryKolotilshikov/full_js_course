db7d10eefcd28f798bd450873b82e68c6dcfc28b
Dmitry_Ko <it_man@mail.ru>
2024-12-13 16:43:54 +0500

    49 fix content (1)

--- 49_fetch_async_await/script.js
+++ 49_fetch_async_await/script.js
@@ -1,38 +1,38 @@
 // fetch, async, await, try, catch, finally
 
-// fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
+fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(response => response.json())
+      .then(json => console.log(json))
 
-// fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
+fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+      .then(response => response.json())
+      .then(json => console.log(json))
 
 
 // -------------- Renders posts ----------------
 
-// fetch('https://jsonplaceholder.typicode.com/posts?_limit=10', {
-//     method: "GET",
-//     headers: {
-//         'Content-type': 'application/json; charset=UTF-8',
-//     }
-// })
-//     .then(response => response.json())
-//     .then(todos => { console.log(todos); renderPosts(todos) })
+fetch('https://jsonplaceholder.typicode.com/posts?_limit=10', {
+    method: "GET",
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    }
+})
+    .then(response => response.json())
+    .then(todos => { console.log(todos); renderPosts(todos) })
 
-// const renderPosts = (todos) => {
-//     todos.forEach(todo => {
-//         const todoContainer = document.createElement("div");
-//         const todoTitle = document.createElement("h3");
-//         const todoBody = document.createElement("p");
+const renderPosts = (todos) => {
+    todos.forEach(todo => {
+        const todoContainer = document.createElement("div");
+        const todoTitle = document.createElement("h3");
+        const todoBody = document.createElement("p");
 
-//         todoTitle.textContent = todo.title;
-//         todoBody.textContent = todo.body;
-//         todoContainer.append(todoTitle, todoBody);
+        todoTitle.textContent = todo.title;
+        todoBody.textContent = todo.body;
+        todoContainer.append(todoTitle, todoBody);
 
-//         document.body.append(todoContainer);
-//     })
-// }
+        document.body.append(todoContainer);
+    })
+}
 
 // -------------- Renders posts (async & await) ----------------
 
