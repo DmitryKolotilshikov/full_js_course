1eefca20c29b674fad76ef7d865f0394efacf588
Dmitry_Ko <it_man@mail.ru>
2024-12-17 13:44:21 +0500

    51 - logic improve

--- 51_form_project/script.js
+++ 51_form_project/script.js
@@ -24,84 +24,24 @@ form.addEventListener("submit", (e) => {
         avatar: formUserData.userImageUrl,
     }
 
-    console.log(newUserData);
     createNewUserAsync(newUserData);
 })
 
 // ------- Заполнение модального окна разметкой формы -------
 const populateDialog = (userId) => {
-    editUserFormDialog.innerHTML = `
-        <button type="button" class="close-edit-form-btn" data-close-edit-form>❌</button>
+    editUserFormDialog.innerHTML = "";
 
-        <form class="form" data-edit-form>
-            <input type="text" name="userId" value="${userId}" hidden/>
+    const editForm = document.createElement("form");
+    const closeFormBtn = document.createElement("button");
 
-            <div class="control-field">
-                <label for="nameId" class="form-label">Name</label>
-                <input type="text" name="userName" id="nameId" class="form-control" required minlength="2"
-                    maxlength="13" />
-            </div>
+    closeFormBtn.classList.add("close-edit-form-btn");
+    closeFormBtn.textContent = "❌";
+    closeFormBtn.addEventListener("click", () => editUserFormDialog.close());
 
-            <div class="control-field">
-                <label for="cityId" class="form-label">City</label>
-                <input type="text" name="userCity" id="cityId" class="form-control" required minlength="2"
-                    maxlength="30" />
-            </div>
-
-            <div class="control-field">
-                <label for="emailId" class="form-label">Email</label>
-                <input type="email" name="userEmail" id="emailId" class="form-control form-control--email"
-                    required />
-            </div>
-
-            <div class="control-field">
-                <label for="imagesUrlId" class="form-label">Email</label>
-
-                <select name="userImageUrl" id="imagesUrlId" class="form-control form-control--images" required>
-                    <option value="">Image URL</option>
-                    <hr>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=88cc30ba21222ee61db2d32974a5b380259ee41f-3380069-images-thumbs&n=13">
-                        Cat 1</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=6444bd82bce43803b8ad0601c12a80e7-5230955-images-thumbs&n=13">
-                        Cat 2</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=60f5028735fd33706fd8e50bb1d7f636062b21a4-8210619-images-thumbs&n=13">
-                        Cat 3</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=ae0521f7a56e37beaa15c3469ab4c338e350c501-4453150-images-thumbs&n=13">
-                        Dog 1</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=fec854b859968252cfa2ac789041838662475e7e-4667938-images-thumbs&n=13">
-                        Dog 2</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=26253ff7e734e6fd0431b2fbc2b4a1a669ed2685be8a39d1-9148232-images-thumbs&n=13">
-                        Dog 3</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=eaed52ea5bd298c60ff850710e5d05ddd9d26b49-8082760-images-thumbs&n=13">
-                        Wolf 1</option>
-                    <option
-                        value="https://avatars.mds.yandex.net/i?id=730e0bcc75f17fff296adf3dcdaae2036067665ec12d546e-12645552-images-thumbs&n=13">
-                        Fox 1</option>
-                </select>
-            </div>
-
-            <button type="submit" class="btn submit-btn" data-form-edit-and-submit-btn>Edit User</button>
-        </form>
-    `
-}
-
-// ------- Клик по всему диалогу/модальному окну (делегирование событий) -------
-editUserFormDialog.addEventListener("click", (e) => {
-    if (e.target.hasAttribute("data-close-edit-form")) {
-        editUserFormDialog.close();
-        return;
-    }
-    if (e.target.hasAttribute("data-form-edit-and-submit-btn")) {
+    editForm.addEventListener("submit", (e) => {
         e.preventDefault();
-        const form = document.querySelector("[data-edit-form]");
-        const formData = new FormData(form);
+        const formData = new FormData(editForm);
+
         const formUserData = Object.fromEntries(formData);
 
         const newUserData = {
@@ -113,14 +53,77 @@ editUserFormDialog.addEventListener("click", (e) => {
         }
 
         editExistingUserAsync(newUserData);
-    }
-})
+    })
+
+    editForm.classList.add("form");
+
+    editForm.innerHTML = `
+        <input type="text" name="userId" value="${userId}" hidden/>
+
+        <div class="control-field">
+            <label for="nameId" class="form-label">Name</label>
+            <input type="text" name="userName" id="nameId" class="form-control" required minlength="2"
+                maxlength="13" />
+        </div>
+
+        <div class="control-field">
+            <label for="cityId" class="form-label">City</label>
+            <input type="text" name="userCity" id="cityId" class="form-control" required minlength="2"
+                maxlength="30" />
+        </div>
+
+        <div class="control-field">
+            <label for="emailId" class="form-label">Email</label>
+            <input type="email" name="userEmail" id="emailId" class="form-control form-control--email"
+                required />
+        </div>
+
+        <div class="control-field">
+            <label for="imagesUrlId" class="form-label">Email</label>
+
+            <select name="userImageUrl" id="imagesUrlId" class="form-control form-control--images" required>
+                <option value="">Image URL</option>
+                <hr>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=88cc30ba21222ee61db2d32974a5b380259ee41f-3380069-images-thumbs&n=13">
+                    Cat 1</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=6444bd82bce43803b8ad0601c12a80e7-5230955-images-thumbs&n=13">
+                    Cat 2</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=60f5028735fd33706fd8e50bb1d7f636062b21a4-8210619-images-thumbs&n=13">
+                    Cat 3</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=ae0521f7a56e37beaa15c3469ab4c338e350c501-4453150-images-thumbs&n=13">
+                    Dog 1</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=fec854b859968252cfa2ac789041838662475e7e-4667938-images-thumbs&n=13">
+                    Dog 2</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=26253ff7e734e6fd0431b2fbc2b4a1a669ed2685be8a39d1-9148232-images-thumbs&n=13">
+                    Dog 3</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=eaed52ea5bd298c60ff850710e5d05ddd9d26b49-8082760-images-thumbs&n=13">
+                    Wolf 1</option>
+                <option
+                    value="https://avatars.mds.yandex.net/i?id=730e0bcc75f17fff296adf3dcdaae2036067665ec12d546e-12645552-images-thumbs&n=13">
+                    Fox 1</option>
+            </select>
+        </div>
+
+        <button type="submit" class="btn submit-btn">Edit User</button>
+    `
+
+    editUserFormDialog.append(closeFormBtn, editForm);
+}
+
 
 // ------- Клик по всему контейнеру (делегирование событий) -------
 usersContainer.addEventListener("click", (e) => {
     if (e.target.hasAttribute("data-user-edit-btn")) {
         // console.log("userEditBtn" in e.target.dataset)
         populateDialog(e.target.dataset.userId);
+
         editUserFormDialog.showModal();
         return;
     }
